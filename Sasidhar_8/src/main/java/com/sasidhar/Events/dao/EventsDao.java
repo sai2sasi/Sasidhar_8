40344ebe6fb54762d4ca6d17ce9f115655ed74b7
lenovo <lenovo@idea>
2021-06-18 11:00:21 +0530

    ALmost FInal COmmit

--- Sasidhar_8/src/main/java/com/sasidhar/Events/dao/EventsDao.java
+++ Sasidhar_8/src/main/java/com/sasidhar/Events/dao/EventsDao.java
@@ -1,11 +1,29 @@
 package com.sasidhar.Events.dao;
 
+import java.util.List;
+
+
 import org.springframework.stereotype.Repository;
+
+import com.sasidhar.Events.constants.EventType;
 
 @Repository
 public class EventsDao {
 	
-	public MongoTemplate
+	@Autowired
+	private MongoTemplate mongoTemplate;
+	
+	public <T> List<T> findAll( Class<T> clazz, String collection, EventType type){
+		Query query = new Query();
+		if(type.name().equalsIgnoreCase("ALL")){
+			mongoTemplate.findAll(clazz,collection);
+		}else{
+			query.addCriteria(Criteria.where("eventsType").in(type.name()));
+			mongoTemplate.find(query,clazz,collection);
+		}
+		
+		
+	}
 	
 
 }
